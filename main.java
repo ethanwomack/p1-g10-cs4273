af9daff187177f2254f00456ee8af2e6d9b7717d
FatalityWonder <47227913+FatalityWonder@users.noreply.github.com>
2022-01-25 21:09:09 -0600

    Modify main to contain methods for unit testing

--- main.java
+++ main.java
@@ -7,21 +7,23 @@ public class main {
 	
 	public static void main(String[] args)
 	{
+		// initialize char arrays up to the amount of characters present in each language
 		char[] spanish = new char[54];
 		char[] english = new char[52];
+		
+		// Test for reading in files with characters for both alphabets
 		try
 		{
-		File englishFile = new File(englishFileName);
-		File spanishFile = new File(spanishFileName);
-		Scanner englishScnr = new Scanner(englishFile);
-		Scanner spanishScnr = new Scanner(spanishFile);
+			File englishFile = new File(englishFileName);
+			File spanishFile = new File(spanishFileName);
+			Scanner englishScnr = new Scanner(englishFile);
+			Scanner spanishScnr = new Scanner(spanishFile);
 		
-		english = englishScnr.nextLine().replaceAll(",", "").toCharArray();
-
-		spanish = spanishScnr.nextLine().replaceAll(",","").toCharArray();
+			english = englishScnr.nextLine().replaceAll(",", "").toCharArray();
+			spanish = spanishScnr.nextLine().replaceAll(",", "").toCharArray();
 		
-		englishScnr.close();
-		spanishScnr.close();
+			englishScnr.close();
+			spanishScnr.close();
 		}
 		catch(FileNotFoundException e)
 		{
@@ -33,54 +35,86 @@ public class main {
 		System.out.println("Enter the character and I will tell you what language you are using   \n (Only Supports English and Spanish)");
 		System.out.println("Type QUIT to exit");
 		String input = scnr.nextLine();
-		boolean isEnglish = false;
-		boolean isSpanish = false;	
+			
 		while(!input.equals("QUIT"))
 		{	
-			isEnglish = false;
-			isSpanish = false;
+			// check if character is in english or spanish, neither or both languages
+			int val = checkChar(input.charAt(0), english, spanish);
 			
-			for(int i = 0; i < english.length; i++)
+			// print out what language the character is in
+			switch (val)
 			{
-				if(input.charAt(0) == english[i])
-				{
-					isEnglish = true;
-				}
-			}
-			
-			for(int i = 0; i < spanish.length; i++)
-			{
-				if(input.charAt(0) == spanish[i])
-				{
-					isSpanish = true;
-				}
-			}
-			if(isEnglish)		
-			{
-				if(isSpanish)
-				{
-					System.out.println("The character is both English and Spanish");
-				}
-				else
-				{
+				case 0: // Neither Language
+					System.out.println("The character is neither English or Spanish");
+					break;
+				case 1: // Only English, Should not execute ever
 					System.out.println("The character is English");
-				}
+					break;
+				case 2: // Only Spanish
+					System.out.println("The character is Spanish");
+					break;
+				case 3: // Both Spanish and English
+					System.out.println("The character is both English and Spanish");
+					break;
 			}
 			
-			else if(isSpanish)
-			{
-				System.out.println("The character is Spanish");
-			}
-			else
-			{
-				System.out.println("The character is neither English or Spanish");
-			}
-			
+			// priming read
 			System.out.println("Enter the character and I will tell you what language you are using   \n (Only Supports English and Spanish)");
 			System.out.println("Type QUIT to exit");
 			input = scnr.nextLine();
 		}
+		
 		scnr.close();
 	}
 	
+	/*
+	 * Checks If the character is in english, spanish, neither, or both languages
+	 * Returns 0 if character is in neither language,
+	 * 1 if character is only in english (this should never be returned)
+	 * 2 if character is only in spanish
+	 * 3 if character is in both languages
+	 */
+	public static int checkChar(char input, char[] english, char[] spanish)
+	{
+		boolean isEnglish = checkEnglish(input, english);
+		boolean isSpanish = checkSpanish(input, spanish);
+					
+		if(isEnglish)
+			if(isSpanish) // Both 
+				return 3;
+			else // only english
+				return 1;
+		else if(isSpanish) // only spanish
+			return 2;
+		else // neither
+			return 0;		
+	}
+
+	/*
+	 * Check if character is in the spanish alphabet
+	 * Returns True or False
+	 */
+	private static boolean checkSpanish(char input, char[] spanish) 
+	{
+		for(int i = 0; i < spanish.length; i++)
+			if(input == spanish[i])
+				return true;
+		
+		// not in alphabet
+		return false;
+	}
+
+	/*
+	 * Check if character is in the english alphabet
+	 * Returns True or False
+	 */
+	private static boolean checkEnglish(char input, char[] english) 
+	{
+		for(int i = 0; i < english.length; i++)
+			if(input == english[i])
+				return true;
+		
+		// not in alphabet
+		return false;
+	}
 }
